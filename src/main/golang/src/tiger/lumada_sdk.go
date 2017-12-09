23df223bde4407813597539f4604f1ef7c27466e
hitachivantara <greg.wu@hitachivantara.com>
2017-12-08 17:43:48 -0800

    Lumada GO SDK test

--- src/main/golang/src/tiger/lumada_sdk.go
+++ src/main/golang/src/tiger/lumada_sdk.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-  "fmt"
-  "github.com/hitachivantara/lumada"
+	"fmt"
+	"github.com/hitachivantara/lumada"
 )
 
-
 func main() {
-  fmt.Println("Welcome to Lumada");
+	fmt.Println("Welcome to Lumada")
 
-  asset := lumada.Asset{Id:"9d23824d-5ac1-48e9-8b97-cad607938a8f"}
-  fmt.Println(asset)
+	//asset := lumada.Asset{Id: "9d23824d-5ac1-48e9-8b97-cad607938a8f"}
+	//fmt.Println(asset)
+	
+	loginReq := lumada.LoginRequest{Username:"admin", Password:"RedmondAdmin%5"}
+	lumada.Login(loginReq, "10.0.2.15")
 }
