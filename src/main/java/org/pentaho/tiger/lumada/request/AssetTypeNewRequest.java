dccb961a0f3d9e6f1f2e507050bcbd5c680a7ced
hitachivantara <greg.wu@hitachivantara.com>
2017-12-11 16:43:58 -0800

    Upload picture, create new asset avatar type,

---
+++ src/main/java/org/pentaho/tiger/lumada/request/AssetTypeNewRequest.java
@@ -0,0 +1,22 @@
+package org.pentaho.tiger.lumada.request;
+
+public class AssetTypeNewRequest {
+    private String name;
+    private String pictureId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPictureId() {
+        return pictureId;
+    }
+
+    public void setPictureId(String pictureId) {
+        this.pictureId = pictureId;
+    }
+}
